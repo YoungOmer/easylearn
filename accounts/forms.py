c8b5c067d27c70f2a8e8939de5068aa53bdf9673
bridget001998!= <be.twite6413@gmail.com>
2020-07-07 00:17:37 +0200

    db1

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from accounts.models import User
-#from blog.models import Series
 
 
 class RegisterForm(UserCreationForm):
